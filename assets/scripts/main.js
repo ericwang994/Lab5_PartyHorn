c749cb775bc820cff62317d61dcde2d2b80c7fed
ericwang994 <60726026+ericwang994@users.noreply.github.com>
2021-02-05 20:18:31 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,49 @@
 // main.js
 
 // TODO
+
+
+// Input field that serves as the textual indicator of sound level
+var textInput = document.getElementById("volume-number");
+var slider = document.getElementById("volume-slider");
+
+
+textInput.oninput = function () {
+    value = textInput.value;
+    slider.value = value;
+    button_control(value);
+    soundLevel (value);
+};
+
+slider.oninput = function(){
+    value = slider.value;
+    textInput.value = value;
+    button_control(value);
+    soundLevel (value);
+};
+
+//Volume icon that change depending on the sound level
+
+var honkButton = document.getElementById("honk-btn");
+var volume_icon = document.getElementById("volume-image");
+
+function button_control(val){
+    if(val == 0){
+        honkButton.disabled = true; 
+    }else{
+        honkButton.disabled = false; 
+    }
+}
+
+function soundLevel(val) {
+    if (val == 0) {
+        volume_icon.src = "./assets/media/icons/volume-level-0.svg";
+    } else if (val == 100) {
+        volume_icon.src = "./assets/media/icons/volume-level-3.svg";
+    } else if(val < 67 && val > 34){
+        volume_icon.src = "./assets/media/icons/volume-level-2.svg";
+    } else if(val < 34 && val > 1){
+        volume_icon.src = "./assets/media/icons/volume-level-1.svg";
+    }
+
+};
